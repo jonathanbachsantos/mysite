8fc4508268a4b0d6a17a6fd752e9aec237575c3f
Jonathan Bach <jo.bach.s@gmail.com>
2020-07-25 18:11:11 -0300

    updated link repository

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,7 +79,7 @@ const Navbar = class extends React.Component {
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/jonathanbachsantos"
                 target="_blank"
                 rel="noopener noreferrer"
               >
